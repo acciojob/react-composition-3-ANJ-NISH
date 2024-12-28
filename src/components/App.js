9e83f6fa8df22aeff5afd33e0f78d703536fa891
Anurag Joshi <anuragj990@gmail.com>
2024-12-28 23:09:40 +0530

    2nd try

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import { Tooltip } from "./Tooltip";
 const App = () => {
   return (
     <div>
-      <Tooltip text='This is a tooltip' children={<h3 style={{textDecoration: 'underline', padding: '10px', position: 'relative'}}>Hover over me</h3>}/>
+      <Tooltip text='This is a tooltip' children={<h2 style={{textDecoration: 'underline', padding: '10px', position: 'relative'}}>Hover over me</h2>}/>
        <br/>
        <Tooltip text="This is another tooltip" children={<p style={{textDecoration: 'underline', padding: '10px', position: 'relative'}}>Hover over me to see another tooltip</p>}/>
     </div>
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -10,7 +10,7 @@ export const Tooltip=({text, children})=>
       {show && <div style={{backgroundColor: 'red', color: 'white',fontWeight: '700', height: '80px', width: '100px', 
        position: 'absolute', top: '20px',
       }}>{text}</div>}
-      <div onMouseOut={()=> setShow(false)} onMouseOver={()=>setShow(true)} style={{marginTop: '100px'}}>{children}</div>
+      <div onMouseOut={()=> setShow(false)} onMouseEnter={()=>setShow(true)} onMouseOver={()=>setShow(true)} style={{marginTop: '100px'}}>{children}</div>
     </>
   )
 }
